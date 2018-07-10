87072deee3ef031fc5dca07c7dab306dd1c008c1
chnotebook\admin <chomovva@gmail.com>
2018-07-11 00:13:20 +0300

    v 0.0.9

--- includes/part-info.php
+++ includes/part-info.php
@@ -15,7 +15,20 @@ echo "<div class=\"main__info info\">\r\n";
 if ( get_theme_mod( 'qr_code_url', true ) ) echo "  <img class=\"lazy qr-code\" src=\"#\" data-src=\"http://chart.apis.google.com/chart?choe=UTF-8&amp;chld=L&amp;cht=qr&amp;chs=100x100&amp;chl=" . get_the_permalink( get_the_ID() ) . "\">\r\n";
 echo "  <h1 class=\"title\">" . apply_filters( 'the_title', get_the_title( get_the_ID() ) ) . "</h1>\r\n";
 if ( has_excerpt( get_the_ID() ) ) echo "  <p class=\"lead\">" . get_the_excerpt( get_the_ID() ) . "</p>\r\n";
+
+echo "  <div class=\"clearfix\">\r\n";
+echo "    <div class=\"pull-left\">";
 the_breadcrumb();
+echo "    </div>\r\n"; // .pull-left
+if ( ( is_single() ) || ( is_page() ) ) {
+	echo "    <div class=\"pull-left\">";
+	set_post_views( get_the_ID() );
+	get_post_views( get_the_ID() );
+	echo "    </div>\r\n"; // .pull-left
+}
+echo "  </div>\r\n"; // .clearfix
+
+
 echo "  <div class=\"clearfix\">\r\n";
 
 get_template_part( 'part', 'share' );
